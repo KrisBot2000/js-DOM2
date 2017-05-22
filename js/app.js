d58cecddd56aa8be5a47577dec0fff7e7016a5da
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-05-21 23:19:12 -1000

    added ex 4

--- js/app.js
+++ js/app.js
@@ -31,16 +31,26 @@ Hours: 6pm - 12am Sun - Sat
 
 Append this div element to the div id "text3"*/
 
+var divHours3 = document.createElement("div");
+divHours3.id = "hours3";
+divHours3.innerHTML = "Hours: 6pm - 12am Sun - Sat";
+document.getElementById("text3").appendChild(divHours3);
 
 //4. Kam Bowl
 /*Kam Bowl got no spare time to update their info and business is suffering! Help them regain their kingpin status again.
 
 Create a new div element with an id of "price4". Inside this div element, give it the following info:
 
-Price: $$$
+Price: $$$*/
 
-Append this element to the div with the id of 
+var divPrice4 = document.createElement("div");
+divPrice4.id = "price4";
+divPrice4.innerHTML = "Price: $$$";
+
+/*Append this element to the div with the id of 
 "text4"*/
+
+document.getElementById("text4").appendChild(divPrice4);
 
 /*Create a new div element with an id of "addy4". Inside this div element, give it the following info:
 
@@ -48,6 +58,12 @@ Address: 1620 N. School St, Honolulu, HI 96817
 
 Append this element to the div with the id of 
 "text4"*/
+
+var divAddy4 = document.createElement("div");
+divAddy4.id = "addy4";
+divAddy4.innerHTML = "Address: 1620 N. School St, Honolulu, HI 96817";
+document.getElementById("text4").appendChild(divAddy4);
+
 
 /*Create a new div element with an id of "hours4". Inside this div element, give it the following info:
 
@@ -57,12 +73,21 @@ Append this element to the div with the id of
 "text4"
 */
 
+var divHours4 = document.createElement("div");
+divHours4.id = "hours4";
+divHours4.innerHTML = "Hours: 9am - 10pm Sun - Sat";
+document.getElementById("text4").appendChild(divHours4);
+
 /*Create a paragraph element with an id of "update4". Inside this paragraph element, give it the following info:
 
 I'd leave a blank space for this oxtail soup!
 
 Append this element to the div id of "comment4"*/
 
+var pUpdate4 = document.createElement("p");
+pUpdate4.id = "update4";
+pUpdate4.innerHTML = "I'd leave a blank space for this oxtail soup!";
+document.getElementById("comment4").appendChild(pUpdate4);
 
 //5. The Death Star Cafe
 /*The empire strikes back with a new and improved oxtail soup offering. Help the dark side win back customers by executing the following commands:*/
